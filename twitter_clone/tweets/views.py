76f35cb774f0e147067d6e1fa6b6ba5db0a1059b
gprok <gprok60@gmail.com>
2023-06-26 23:38:58 +0300

    week-2 login

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Tweet
+from django.contrib.auth.decorators import login_required
 
 def feed(request):
     all_tweets = Tweet.objects.all()
     return render(request, "feed.html", {'tweets': all_tweets})
+
+
+def home(request):
+    return render(request, "home.html")
+
+@login_required
+def profile(request):
+    return render(request, "profile.html")
--- twitter_clone/twitter_clone/urls.py
+++ twitter_clone/twitter_clone/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from tweets import views
 import tweets
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('feed', tweets.views.feed),
+    path('profile', tweets.views.profile),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', tweets.views.home)
 ]
